17334687ff57e2b7e74ee4c1327568827099b569
Shiphan <140245703+Shiphan@users.noreply.github.com>
2024-10-17 21:43:40 +0800

    add some comment

--- hw1.java
+++ hw1.java
@@ -22,16 +22,22 @@ public class hw1 {
 		Scanner scanner = new Scanner(System.in);
 		int i = 0;
 
+		// 取得使用者的輸入
 		boolean validInput;
+
+		// 直到取得正確的輸入前, 接收輸入
 		do {
 			try {
 				i = scanner.nextInt();
 				validInput = true;
 			} catch (java.util.InputMismatchException e) {
+				// 處理不能作為int的輸入
 				validInput = false;
 				System.out.printf("`%s`不能作為整數或超過整數的容量\n", scanner.next());
 				continue;
 			}
+
+			// 處理不是大於0的輸入
 			if (i <= 0) {
 				validInput = false;
 				System.out.printf("%d不是大於0的整數\n", i);
@@ -39,22 +45,31 @@ public class hw1 {
 		} while (!validInput);
 		scanner.close();
 
+		// 先處理輸入為1的情況
 		if (i == 1) {
 			System.out.println("否");
 			System.out.println("1沒有質因數");
 			return;
 		}
 
+		// 創造一個ArrayList儲存質因數
 		ArrayList<Integer> factors = new ArrayList<Integer>();
+
+		// 儲存剩下需要處理的數字
 		int remain = i;
 
+		// 重複直到找到所有質因數
 		while (remain != 1) {
+			// 要從多大的數字開始嘗試, 預設為2, 若前面已經有算出的質因數, 則從前一個算出的質因數開始
 			int num = 2;
 			if (!factors.isEmpty()) {
 				num = factors.getLast();
 			}
+
+			// 從起始數字開始, 檢查該數字使否是目標的因數
 			for (; num <= remain; num++) {
 				if (remain % num == 0) {
+					// 若是因數, 將這個數字儲存起來
 					factors.add(num);
 					remain = remain / num;
 					break;
@@ -62,14 +77,15 @@ public class hw1 {
 			}
 		}
 
+		// 若只有一個質因數, 則輸入是質數
 		if (factors.size() == 1) {
 			System.out.println("是");
 		} else {
 			System.out.println("否");
 		}
 
+		// 輸出所有質因數
 		System.out.printf("%d = ", i);
-
 		System.out.println(String.join(" * ", factors.stream().map((f) -> Integer.toString(f)).toList()));
 	}
 }
